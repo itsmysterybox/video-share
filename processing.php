e27cf1e2b75f03d1e667f265048e5b0c51269e87
Prateek Dagur <deviscoding@gmail.com>
2019-03-18 22:41:34 +0530

    disallow html tags in description

--- processing.php
+++ processing.php
@@ -2,17 +2,26 @@
 require_once("includes/header.php");
 require_once("includes/classes/VideoUploadData.php");
 require_once("includes/classes/VideoProcessor.php");
+require_once('htmlpurifier/HTMLPurifier.standalone.php');
 
 if (!isset($_POST["uploadButton"])) {
 	echo "No file sent to page.";
 	exit();
 }
 
+function purifyHTML($input) {
+	
+	$config = HTMLPurifier_Config::createDefault();
+	$config->set('HTML.Allowed', '');
+	$purifier = new HTMLPurifier($config);
+	return $purifier->purify($input);
+}
+
 // 1. Create file upload data
 $videoUploadData = new VideoUploadData(
 							$_FILES["fileInput"],
 							$_POST["titleInput"],
-							$_POST["descriptionInput"],
+							purifyHTML($_POST["descriptionInput"]),
 							$_POST["privacyInput"],
 							$_POST["categoryInput"],
 							$userLoggedInObj->getUsername()
