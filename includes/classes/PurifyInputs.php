04e44226c6ac849b6fdf45edc9b3300f0c3ad874
Prateek Dagur <deviscoding@gmail.com>
2019-03-22 18:34:13 +0530

    replace bad words with *

--- includes/classes/PurifyInputs.php
+++ includes/classes/PurifyInputs.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/Projects/VideoTube/htmlpurifier/HTMLPurifier.standalone.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/VideoTube/htmlpurifier/HTMLPurifier.standalone.php');
 class PurifyInputs {
 	
 	public static function removeHtmlTags($input) {		
@@ -26,8 +26,48 @@ class PurifyInputs {
 	    return $text;
 	}
 
-	// Detect words enclosed within *, _, -, 
-
 	// Detect and remove abusive words
+	public static function removeAbusiveWords($input) {
+		$abusive_words = file('../bad_words.txt');
+		$replaceWith = '*';
+
+		$replace = array();
+		$replace['a'] = '(a|a\.|a\-|4|@|Á|á|À|Â|à|Â|â|Ä|ä|Ã|ã|Å|å|α|Δ|Λ|λ)';
+		$replace['b'] = '(b|b\.|b\-|8|\|3|ß|Β|β)';
+		$replace['c'] = '(c|c\.|c\-|Ç|ç|¢|€|<|\(|{|©)';
+		$replace['d'] = '(d|d\.|d\-|&part;|\|\)|Þ|þ|Ð|ð)';
+		$replace['e'] = '(e|e\.|e\-|3|€|È|è|É|é|Ê|ê|∑)';
+		$replace['f'] = '(f|f\.|f\-|ƒ)';
+		$replace['g'] = '(g|g\.|g\-|6|9)';
+		$replace['h'] = '(h|h\.|h\-|Η)';
+		$replace['i'] = '(i|i\.|i\-|!|\||\]\[|]|1|∫|Ì|Í|Î|Ï|ì|í|î|ï)';
+		$replace['j'] = '(j|j\.|j\-)';
+		$replace['k'] = '(k|k\.|k\-|Κ|κ)';
+		$replace['l'] = '(l|1\.|l\-|!|\||\]\[|]|£|∫|Ì|Í|Î|Ï)';
+		$replace['m'] = '(m|m\.|m\-)';
+		$replace['n'] = '(n|n\.|n\-|η|Ν|Π)';
+		$replace['o'] = '(o|o\.|o\-|0|Ο|ο|Φ|¤|°|ø)';
+		$replace['p'] = '(p|p\.|p\-|ρ|Ρ|¶|þ)';
+		$replace['q'] = '(q|q\.|q\-)';
+		$replace['r'] = '(r|r\.|r\-|®)';
+		$replace['s'] = '(s|s\.|s\-|5|\$|§)';
+		$replace['t'] = '(t|t\.|t\-|Τ|τ)';
+		$replace['u'] = '(u|u\.|u\-|υ|µ)';
+		$replace['v'] = '(v|v\.|v\-|υ|ν)';
+		$replace['w'] = '(w|w\.|w\-|ω|ψ|Ψ)';
+		$replace['x'] = '(x|x\.|x\-|Χ|χ)';
+		$replace['y'] = '(y|y\.|y\-|¥|γ|ÿ|ý|Ÿ|Ý)';
+		$replace['z'] = '(z|z\.|z\-|Ζ)';
+
+		$replacement = array();
+		$abusive_words = array_map('trim', $abusive_words);
+		$whiteListCount = count($abusive_words);
+
+		for ($x = 0; $x < $whiteListCount; $x++) {
+		    $replacement[$x] = str_repeat($replaceWith, strlen($abusive_words[$x]));
+		    $abusive_words[$x] = '/' . str_ireplace(array_keys($replace), array_values($replace), $abusive_words[$x]) . '/i';
+		}
+		return preg_replace($abusive_words, $replacement, $input);
+	}
 }
 ?>
